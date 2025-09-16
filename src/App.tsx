9df9167129378b0fb7d2459c5f869e01ec1f605e
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-09-16 16:46:14 +0700

    Add initial application structure with CSS and main components

---
+++ src/App.tsx
@@ -0,0 +1,94 @@
+// import TypingEffect from './components/animation/typing-effect';
+// import SequentialTypingEffect from './components/hooks/sequential-typing-effect';
+// import { useIsMobile } from './components/hooks/no-mobile';
+import { useEffect, useRef, useState } from 'react';
+import './App.css'
+import { useIsMobile } from './components/hooks/no-mobile';
+import Intro from './components/pages/Intro';
+import Textbox from './components/pages/Textbox';
+import MusicNotifier from './components/hooks/music-notifier';
+import { cn } from './lib/utils';
+
+// function App() {
+//   const isMobile = useIsMobile();
+//   if (isMobile) {
+//     return (
+//       <div className="flex">
+//         <p className='text-[25px]'>Maaf, web ini tidak dapat diakses melalui perangkat mobile.</p>
+//       </div>
+//     );
+//   }
+//   const phrases = ["Halo bang", "Aku cuma nyoba nyoba aja bang", "Serius"];
+//   return (
+//     <>
+//       <div>
+//         <h1 className='text-3xl'>
+//           <SequentialTypingEffect phrases={phrases} />
+//         </h1>
+//       </div>
+//     </>
+//   )
+// }
+
+// function App() {
+//   return (
+//     <div className={cn(
+//         "App",
+//         "h-screen w-screen flex justify-center items-center",
+//         // "bg-[url('/img/sunset.jpg')] bg-cover bg-[position:center_bottom] bg-no-repeat bg-fixed"
+//         "bg-zinc-400"
+//       )}>
+//       <main>
+//         <div className="flex justify-center items-center">
+//           {/* <TheBook /> */}
+//           <Intro />
+//         </div>
+//       </main>
+//     </div>
+//   );
+// }
+
+function App() {
+  const isMobile = useIsMobile();
+  const [showConfess, setShowConfess] = useState(false);
+ 
+  const audioRef = useRef<HTMLAudioElement>(null);
+  
+  useEffect(() => {
+    if (showConfess) {
+      audioRef.current?.play().catch(error => {
+        console.error("Audio play was prevented:", error);
+      });
+    }
+  }, [showConfess]);
+
+  const handleIntroComplete = () => {
+    setShowConfess(true);
+  };
+
+
+  return (
+    <div className={cn(
+      "App",
+      "h-screen flex justify-center items-center",
+      "bg-black",
+    )}>
+      <main className='w-full h-full flex justify-center items-center overflow-hidden'>
+        {/* 3. Gunakan kondisi di dalam JSX untuk menentukan apa yang akan dirender. */}
+        {isMobile ? (
+          <div className="justify-center items-center p-5 flex">
+            <p className='text-white text-center text-[25px]'>Hai, aku cuma mau nyaranin, buka websitenya lewat laptop yaa, biar pengalaman membacanya lebih nyaman.</p>
+          </div>
+        ) : showConfess ? (
+          <Textbox />
+        ) : (
+          <Intro onComplete={handleIntroComplete} />
+        )}
+      </main>
+      {/* <audio ref={audioRef} src="/music/undertale.mp3" loop /> */}
+      <MusicNotifier isPlaying={showConfess} trackName="Toby Fox - It's TV Time!" />
+    </div>
+  );
+}
+
+export default App;
